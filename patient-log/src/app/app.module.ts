37789ef5a4aca49976a762a66074d48b28e564e8
Nirmal Dinesh <nirmalndinesh@gmail.com>
2023-07-02 21:20:25 +0530

    Edited patient screen and top nav

--- patient-log/src/app/app.module.ts
+++ patient-log/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PatientsScreenComponent } from './patients-screen/patients-screen.component';
 import { SideNavBarComponent, TopNavBarComponent  } from './nav-bars/nav-bars.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,10 @@ import { SideNavBarComponent, TopNavBarComponent  } from './nav-bars/nav-bars.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
