2279a3907c6c6f65fb00fe2e6f6ade2a5bac0287
Nirmal Dinesh <nirmalndinesh@gmail.com>
2023-07-08 22:06:07 +0530

    Created test-dev feature branch

---
+++ patient-log/src/app/temp/temp.component.ts
@@ -0,0 +1,31 @@
+import { AutofillMonitor } from '@angular/cdk/text-field';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-temp',
+  templateUrl: './temp.component.html',
+  styleUrls: ['./temp.component.css']
+})
+export class TempComponent implements AfterViewInit, OnDestroy{
+  
+  @ViewChild('first', {read: ElementRef}) firstName!: ElementRef<HTMLElement>;
+  @ViewChild('last', {read: ElementRef}) lastName!: ElementRef<HTMLElement>;
+  firstNameAutofilled!: boolean;
+  lastNameAutofilled!: boolean;
+
+  constructor(private _autofill: AutofillMonitor) {}
+
+  ngAfterViewInit() {
+    this._autofill
+      .monitor(this.firstName)
+      .subscribe(e => (this.firstNameAutofilled = e.isAutofilled));
+    this._autofill
+      .monitor(this.lastName)
+      .subscribe(e => (this.lastNameAutofilled = e.isAutofilled));
+  }
+
+  ngOnDestroy() {
+    this._autofill.stopMonitoring(this.firstName);
+    this._autofill.stopMonitoring(this.lastName);
+  }
+}
