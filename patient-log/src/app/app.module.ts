87a113a41fc4d92f7088f4958ea130e26c5177d9
Nirmal Dinesh <nirmalndinesh@gmail.com>
2023-07-08 16:08:53 +0530

    Update patient screen

--- patient-log/src/app/app.module.ts
+++ patient-log/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
+// **************** ngMaterial imports ***************************
+import { MatDividerModule} from '@angular/material/divider';
+// import { MatListModule} from '@angular/material/list';
+// import {MatMenuModule} from '@angular/material/menu';
+// import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+// ***************************************************************
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -9,7 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { PatientsScreenComponent } from './patients-screen/patients-screen.component';
 import { SideNavBarComponent, TopNavBarComponent  } from './nav-bars/nav-bars.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,14 +23,19 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
     HomepageComponent,
     PatientsScreenComponent,
     SideNavBarComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatDividerModule,
-    MatListModule
+    // MatListModule,
+    // MatMenuModule,
+    // MatButtonModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- patient-log/src/app/patients-screen/patients-screen.component.ts
+++ patient-log/src/app/patients-screen/patients-screen.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-patients-screen',
   templateUrl: './patients-screen.component.html',
-  styleUrls: ['./patients-screen.component.css']
+  styleUrls: ['./patients-screen.component.css'],
+  // standalone: true,
+  // imports: [MatButtonModule, MatMenuModule]
 })
 export class PatientsScreenComponent {
 
+  name: any
 }
